19c1e54992945e948100179e31f967dcd278502f
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2022-10-03 18:02:13 +0200

    Fix blinking on initial load

--- src/components/OnboardingProtectionRoute.tsx
+++ src/components/OnboardingProtectionRoute.tsx
@@ -1,42 +1,26 @@
 import {useRouter} from 'next/router'
 import React from 'react'
 import {useAuth} from '../AuthUserContext'
-import {QueryGuard} from '../QueryGuard'
 import {useGetUser} from './api/user'
-import Loading from './Loading'
 
 const routesThatDontNeedOnBoardingProtection = ['/login', '/onboarding']
 
 const OnboardingProtectionRoute = ({children}: {children: React.ReactNode}) => {
-  const {user, isLoading} = useAuth()
+  const {user} = useAuth()
   const useGetSocUser = useGetUser(!!user)
   const router = useRouter()
 
-  if (isLoading) {
-    return <Loading />
-  }
-
   if (
     user &&
+    !useGetSocUser.isLoading &&
+    !useGetSocUser.data &&
     !routesThatDontNeedOnBoardingProtection.includes(router.pathname)
   ) {
-    return (
-      <QueryGuard {...useGetSocUser}>
-        {(socUser) => {
-          if (socUser) {
-            // socUser exists, we can return content
-            return <>{children}</>
-          } else {
-            // socUser does not exist but user is logged in, it means he has to onboard
-            router.replace({
-              pathname: '/onboarding',
-              query: {from: location.pathname},
-            })
-            return <></>
-          }
-        }}
-      </QueryGuard>
-    )
+    router.replace({
+      pathname: '/onboarding',
+      query: {from: location.pathname},
+    })
+    return <></>
   }
 
   return <>{children}</>
