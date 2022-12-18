4fff1246734068940e34a09a77c4e8067baa4a6f
xjahic <jahic.jakub@gmail.com>
2022-12-18 14:13:54 +0100

    Hopefully fixing hydration errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,6 @@ import OnboardingProtectionRoute from '../components/OnboardingProtectionRoute'
 import SSRWrapper from '../components/SSRWrapper'
 import {useTheme} from '../hooks/useTheme'
 import {storage} from '../localStorage'
-import Loading from '../components/Loading'
 import NewsletterModal from '../components/NewsletterModal'
 import '../theme/animations/HeroAnimation.css'
 import 'pure-react-carousel/dist/react-carousel.es.css'
@@ -86,11 +85,8 @@ function MyApp({Component, pageProps}: AppProps) {
                     )}
                     SSRChildren={() => (
                       <>
-                        <div style={{display: 'none'}}>
-                          <Component {...pageProps} />
-                          <Footer />
-                        </div>
-                        <Loading />
+                        <Component {...pageProps} />
+                        <Footer />
                       </>
                     )}
                   />
