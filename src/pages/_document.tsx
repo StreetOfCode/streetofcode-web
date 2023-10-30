09ad9072afd167ce483c8bd86434b4aa1ed4deba
xjahic <jahic.jakub@gmail.com>
2023-10-30 22:42:41 +0100

    Add GTM scripts to document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -115,9 +115,28 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function (w, d, s, l, i) {
+                    w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                    var f = d.getElementsByTagName(s)[0], j = d.createElement(s),
+                    dl = l != 'dataLayer' ? '&l=' + l : '';
+                    j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                    f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');`,
+            }}
+          />
           <link rel="stylesheet" href="https://use.typekit.net/vve8dub.css" />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe
+              src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}"
+              height="0" width="0"
+              style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
           <script dangerouslySetInnerHTML={{__html: codeToRunOnClient}} />
           <Main />
           <NextScript />
